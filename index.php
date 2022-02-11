ffffc437a9fb571df5da88865db4a521fa50e6a4
jordangarcia28 <69139149+jordangarcia28@users.noreply.github.com>
2022-02-11 20:23:18 +0700

    Update index.php

--- index.php
+++ index.php
@@ -26,7 +26,7 @@
 		$result = $conn->query($sql);
 	
 		$users=$result->num_rows;
-		echo "Jumlah user: ".$users."<br>";
+		echo "hehehehe Jumlah user: ".$users."<br>";
 		
 		if ($result->num_rows > 0) {
 		    echo "<table><tr><th>ID</th><th>Name</th><th>Alamat</th><th>Jabatan</th></tr>";
